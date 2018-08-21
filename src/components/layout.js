908711e14cc5e602f68db2282df96cbd3aca3dc8
Jani Eväkallio <jani.evakallio@gmail.com>
2018-08-21 17:56:17 +0100

    Fix page metadata

--- src/components/layout.js
+++ src/components/layout.js
@@ -22,8 +22,8 @@ const Layout = ({ children, data }) => (
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            { name: 'description', content: 'Formidasauce contributions' },
+            { name: 'keywords', content: 'Formidable, Sauce, Open Source' },
           ]}
         >
           <html lang="en" />
